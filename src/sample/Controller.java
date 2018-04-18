37cb7a9788fef0f04d274fe3267bb2b598134e5a
quest1onmark <32542628+quest1onmark@users.noreply.github.com>
2018-04-19 00:47:13 +0200

    Nearly finished Login
    Added key features to the login screen.
    Not done yet.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,7 +4,12 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
 
 public class Controller
 {
@@ -19,10 +24,43 @@ public class Controller
     @FXML
     JFXButton btnDevLogin = null;
 
+    @FXML
+    ImageView imageExit = null;
+
     public Controller()
     {
 
     }
 
+    @FXML
+    private void closeButtonAction()
+    {
+        Stage stage = (Stage) imageExit.getScene().getWindow();
+        stage.close();
+    }
 
+    @FXML
+    private void closeButtonEnter()
+    {
+        Scene scene = imageExit.getScene();
+        scene.setCursor(Cursor.HAND);
+    }
+
+    @FXML
+    private void closeButtonLeave()
+    {
+        Scene scene = imageExit.getScene();
+        scene.setCursor(Cursor.DEFAULT);
+    }
+
+    @FXML
+    private void devLoginPressed()
+    {
+        ChangeFormToGame();
+    }
+
+    private void ChangeFormToGame()
+    {
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,22 +1,42 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root, 300, 440));
+        primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
+
+        EnableDragDrop(root, primaryStage);
     }
 
+    private void EnableDragDrop(Parent root, Stage primaryStage)
+    {
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+        });
+    }
 
     public static void main(String[] args) {
         launch(args);
